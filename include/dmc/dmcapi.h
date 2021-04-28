ee509890076baea158432907de36a0331ffd2640
Freark van der Berg <fivanderberg@gmail.com>
2021-04-28 15:04:26 +0200

    DMC API: adds dmc_insertB(), dmc_deltaB(), and dmc_getpartB()
    These are interfaces that operate on BYTES instead of SLOTS. Currently their implementation is a wrapper around the SLOT versions, meaning their performance can be improved.

--- include/dmc/dmcapi.h
+++ include/dmc/dmcapi.h
@@ -21,10 +21,13 @@ uint64_t dmc_nextstates(void* ctx, StateID stateID);
 StateID dmc_initialstate(void* ctx);
 
 StateID  dmc_insert    (void* ctx, StateSlot* data, uint32_t length, uint32_t isRoot);
+StateID  dmc_insertB   (void* ctx, StateSlot* data, uint32_t lengthInBytes, uint32_t isRoot);
 StateID  dmc_transition(void* ctx, StateID stateID, StateSlot* data, uint32_t length, uint32_t isRoot);
 StateID  dmc_delta     (void* ctx, StateID stateID, uint32_t offset, StateSlot* data, uint32_t length, uint32_t isRoot);
+StateID  dmc_deltaB    (void* ctx, StateID stateID, uint32_t offsetInBytes, StateSlot* data, uint32_t lengthInBytes, uint32_t isRoot);
 uint64_t dmc_get       (void* ctx, StateID stateID, StateSlot* data, uint32_t isRoot);
 uint64_t dmc_getpart   (void* ctx, StateID stateID, uint32_t offset, StateSlot* data, uint32_t length, uint32_t isRoot);
+uint64_t dmc_getpartB  (void* ctx, StateID stateID, uint32_t offsetInBytes, StateSlot* data, uint32_t lengthInBytes, uint32_t isRoot);
 
 uint64_t dmc_big_nextstates(void* ctx, BigStateID stateID);
 StateID dmc_big_initialstate(void* ctx);
--- libdmccapi.cpp
+++ libdmccapi.cpp
@@ -15,6 +15,34 @@ extern "C" StateID  dmc_insert(void* ctx_, StateSlot* data, uint32_t length, uin
     return r.getData();
 }
 
+extern "C" StateID  dmc_insertB(void* ctx_, StateSlot* data, uint32_t lengthInBytes, uint32_t isRoot) {
+    auto ctx = reinterpret_cast<VContext<llmc::storage::StorageInterface>*>(ctx_);
+    llmc::storage::StorageInterface::StateID r;
+    //    printf("dmc_insertB(%p, %p, %u, %u)\n", ctx_, data, length, isRoot); fflush(stdout);
+
+    size_t len_remainder = (lengthInBytes & (sizeof(llmc::storage::StorageInterface::StateSlot) - 1));
+    size_t length = lengthInBytes / sizeof(llmc::storage::StorageInterface::StateSlot);
+
+    if(len_remainder == 0) {
+        if(isRoot) {
+            r = ctx->mc->newState(ctx, 0, length, (llmc::storage::StorageInterface::StateSlot*) data).getState();
+        } else {
+            r = ctx->mc->newSubState(ctx, length, data);
+        }
+    } else {
+        StateSlot tempSlots[length+1];
+        tempSlots[length] = 0;
+        memcpy(tempSlots, data, lengthInBytes);
+        if(isRoot) {
+            r = ctx->mc->newState(ctx, 0, length, tempSlots).getState();
+        } else {
+            r = ctx->mc->newSubState(ctx, length, tempSlots);
+        }
+    }
+    assert(r.getData() && "0 is not a valid chunk ID");
+    return r.getData();
+}
+
 extern "C" StateID  dmc_transition(void* ctx_, StateID stateID, StateSlot* data, uint32_t length, uint32_t isRoot) {
     auto ctx = reinterpret_cast<VContext<llmc::storage::StorageInterface>*>(ctx_);
     llmc::storage::StorageInterface::StateID r;
@@ -41,6 +69,48 @@ extern "C" StateID  dmc_delta(void* ctx_, StateID stateID, uint32_t offset, Stat
     return r.getData();
 }
 
+extern "C" StateID  dmc_deltaB(void* ctx_, StateID stateID, uint32_t offsetInBytes, StateSlot* data, uint32_t lengthInBytes, uint32_t isRoot) {
+    auto ctx = reinterpret_cast<VContext<llmc::storage::StorageInterface>*>(ctx_);
+    llmc::storage::StorageInterface::StateID r;
+//    printf("dmc_deltaB(%p, %zx, %u, %p, %u, %u)\n", ctx_, stateID, offset, data, length, isRoot); fflush(stdout);
+
+    size_t offset_remainder = (offsetInBytes & (sizeof(llmc::storage::StorageInterface::StateSlot) - 1));
+    size_t len_remainder = (lengthInBytes & (sizeof(llmc::storage::StorageInterface::StateSlot) - 1));
+
+    size_t offset = offsetInBytes / sizeof(llmc::storage::StorageInterface::StateSlot);
+
+    if(offset_remainder == 0 && len_remainder == 0) {
+        size_t length = lengthInBytes / sizeof(llmc::storage::StorageInterface::StateSlot);
+        if(isRoot) {
+            r = ctx->mc->newTransition( ctx
+                                      , offset
+                                      , length
+                                      , (llmc::storage::StorageInterface::StateSlot*) data
+                                      , TransitionInfoUnExpanded::None()
+                                      );
+        } else {
+            r = ctx->mc->newSubState( ctx
+                                    , stateID
+                                    , offset
+                                    , length
+                                    , (llmc::storage::StorageInterface::StateSlot*) data
+                                    );
+        }
+    } else {
+        size_t nrSlots = (offset_remainder + lengthInBytes + (sizeof(llmc::storage::StorageInterface::StateSlot) - 1)) / (sizeof(llmc::storage::StorageInterface::StateSlot));
+        StateSlot tempSlots[nrSlots];
+        ctx->mc->getStatePartial(ctx, stateID, offset, tempSlots, nrSlots, isRoot);
+        memcpy(((char*)tempSlots) + offset_remainder, data, lengthInBytes);
+        if(isRoot) {
+            r = ctx->mc->newTransition(ctx, offset, nrSlots, tempSlots, TransitionInfoUnExpanded::None());
+        } else {
+            r = ctx->mc->newSubState(ctx, stateID, offset, nrSlots, tempSlots);
+        }
+    }
+    assert(r.getData() && "0 is not a valid chunk ID");
+    return r.getData();
+}
+
 extern "C" uint64_t dmc_get(void* ctx_, StateID stateID, StateSlot* data, uint32_t isRoot) {
     auto ctx = reinterpret_cast<VContext<llmc::storage::StorageInterface>*>(ctx_);
 //    printf("dmc_get(%p, %zx, %p, %u)\n", ctx_, stateID, data, isRoot); fflush(stdout);
@@ -51,4 +121,23 @@ extern "C" uint64_t dmc_getpart(void* ctx_, StateID stateID, uint32_t offset, St
     auto ctx = reinterpret_cast<VContext<llmc::storage::StorageInterface>*>(ctx_);
 //    printf("dmc_getpart(%p, %zx, %u, %p, %u, %u)\n", ctx_, stateID, offset, data, length, isRoot); fflush(stdout);
     return ctx->mc->getStatePartial(ctx, stateID, offset, data, length, isRoot);
+}
+
+extern "C" uint64_t dmc_getpartB(void* ctx_, StateID stateID, uint32_t offsetInBytes, StateSlot* data, uint32_t lengthInBytes, uint32_t isRoot) {
+    auto ctx = reinterpret_cast<VContext<llmc::storage::StorageInterface>*>(ctx_);
+//    printf("dmc_getpart(%p, %zx, %u, %p, %u, %u)\n", ctx_, stateID, offset, data, length, isRoot); fflush(stdout);
+    size_t offset_remainder = (offsetInBytes & (sizeof(llmc::storage::StorageInterface::StateSlot) - 1));
+    size_t len_remainder = (lengthInBytes & (sizeof(llmc::storage::StorageInterface::StateSlot) - 1));
+
+    size_t offset = offsetInBytes / sizeof(llmc::storage::StorageInterface::StateSlot);
+    if(offset_remainder == 0 && len_remainder == 0) {
+        size_t length = lengthInBytes / sizeof(llmc::storage::StorageInterface::StateSlot);
+        return ctx->mc->getStatePartial(ctx, stateID, offset, data, length, isRoot);
+    } else {
+        size_t nrSlots = (offset_remainder + lengthInBytes + (sizeof(llmc::storage::StorageInterface::StateSlot) - 1)) / (sizeof(llmc::storage::StorageInterface::StateSlot));
+        StateSlot tempSlots[nrSlots];
+        auto r = ctx->mc->getStatePartial(ctx, stateID, offset, tempSlots, nrSlots, isRoot);
+        memcpy(data, ((char*)tempSlots) + offset_remainder, lengthInBytes);
+        return r;
+    }
 }
